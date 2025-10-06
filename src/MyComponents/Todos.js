39dc4c8e35eb0cc8abfdf3d927ff072e89ec7730
Vicky Saroj <sarojvicky101@gmail.com>
2025-10-06 08:39:26 +0530

    Update Todos.js

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -9,7 +9,7 @@ const Todos = (props) =>
         return(
         <div className="container" style={stl}>
         <h3 className="my-3" >Todos List</h3>  
-        {props.todos.length===0 ? "not todo to display." :
+        {props.todos.length===0 ? "no todo to display." :
         props.todos.map((todo)=>
             {    
             return(<TodoItems todos={todo} key={todo.srno} del={props.d}/>)
